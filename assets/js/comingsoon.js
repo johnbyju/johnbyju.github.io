de01fedb1c61bc097435ed9ad089ae66bba6e04b
Johnbyju <138657639+johnbyju@users.noreply.github.com>
2024-07-10 03:59:24 +0530

    Update comingsoon.js

--- assets/js/comingsoon.js
+++ assets/js/comingsoon.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("jul 12, 2024 00:00:00").getTime();
+    const countDate = new Date("jul 15, 2024 00:00:00").getTime();
 
     const currentTime = new Date().getTime();
     console.log(currentTime);
